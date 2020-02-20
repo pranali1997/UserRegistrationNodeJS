a58bdebab5ae88475d70fc153035ee7896d6d295
pranali1997 <pranali.lembhe77@gmail.com>
2020-02-20 12:56:01 +0530

    Refactored : code to follow DRY principle make one function for all different functions

--- source/main/UserRegistration.js
+++ source/main/UserRegistration.js
@@ -1,107 +1,46 @@
 module.exports = {
 
-    firstNameReg(firstName) {
-        var firstNamePattern = "^[A-Z]{1}[a-z]{2,}$"
-
-        if (typeof (firstName) == undefined) {
+    userRegistration(name, detailType) {
+        if (typeof (name) == undefined) {
             return false;
         }
-        if (firstName == undefined) {
+        if (name == undefined) {
             return false;
         }
-        if (typeof (firstName) == Number) {
+        if (typeof (name) == Number) {
             return false;
         }
-        if (firstName.match(firstNamePattern)) {
-            return true;
-        }
-        else
-            return false;
-
-    },
-
-
-    lastNameReg(lastName) {
-        var lastNamePattern = "^[A-Z]{1}[a-z]{2,}$"
-
-        if (typeof (lastName) == undefined) {
-            return false;
-        }
-        if (lastName == undefined) {
-            return false;
-        }
-        if (typeof (lastName) == Number) {
-            return false;
-        }
-        if (lastName.match(lastNamePattern)) {
-            return true;
-        }
-        else
-            return false;
-
-
-    },
-
-    emailRegistration(email){
-        var emailIdPattern="^[0-9a-zA-Z]+([.+_-]?[0-9a-zA-Z]+)*([@][0-9a-zA-Z]+){1}([.][a-zA-Z]{2,3}){1,2}$";
-        if (typeof (email) == undefined) {
-            return false;
-        }
-        if (email == undefined) {
-            return false;
-        }
-        if (typeof (email) == Number) {
-            return false;
-        }
-        
-        if (email.match(emailIdPattern)) {
-            return true;
-        }
-        else
-            return false;
-        
-    },
-
-    emailReg(email){
-        var emailIdPattern="^[0-9a-zA-Z]+([.+_-]?[0-9a-zA-Z]+)*([@][0-9a-zA-Z]+){1}([.][a-zA-Z]{2,3}){1,2}$";
-    
-        for (var email in emailIds) {  
-            if (email.match(emailIdPattern)) {
+        if (detailType == "firstName") {
+            var firstNamePattern = "^[A-Z]{1}[a-z]{2,}$"
+            if (name.match(firstNamePattern)) {
                 return true;
-            }      
-            
-            else{
-                return false;                
             }
         }
-        if (email.match(emailIdPattern)) {
-            return true;
+        if (detailType == "lastName") {
+            var lastNamePattern = "^[A-Z]{1}[a-z]{2,}$"
+            if (name.match(lastNamePattern)) {
+                return true;
+            }
         }
+        if (detailType == "email") {
+            var emailIdPattern = "^[0-9a-zA-Z]+([.+_-]?[0-9a-zA-Z]+)*([@][0-9a-zA-Z]+){1}([.][a-zA-Z]{2,3}){1,2}$";
+            if (name.match(emailIdPattern)) {
+                return true;
+            }
+        }
+        if (detailType == "password") {
+            
+            var passwordPattern = "((?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{7,})*[+@#$%^&*!.()-]{1}";
+            if (name.match(passwordPattern)) {
+                return true;
+            }
+            if(typeof(name)!=NaN){
+                return false
+            }
+        }
+
         else
             return false;
-
-    },
-
-    passWordReg(password){
-        var passwordPattern="((?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{7,})*[+@#$%^&*!.()-]{1}";
-
-        if (typeof (password) == undefined) {
-            return false;
-        }
-        if (password == undefined) {
-            return false;
-        }
-        if (typeof (password) == Number) {
-            return false;
-        }
-        if (password.match(passwordPattern)) {
-            return true;
-        }
-        else
-            return false;
-
 
     }
-
-    
 }
--- source/test/UserRegistration.Test.js
+++ source/test/UserRegistration.Test.js
@@ -5,130 +5,130 @@ var userReg=require('../main/UserRegistration.js');
 
 describe('First Name Validation',function(){
     it('given parameter of function is empty should return false',function(){
-        assert.equal(false,userReg.firstNameReg());
+        assert.equal(false,userReg.userRegistration());
     }),
     it('given first name is null should return false',function(){
-        assert.equal(false,userReg.firstNameReg(null));
+        assert.equal(false,userReg.userRegistration(null,"firstName"));
     }),
     it('given first name is empty should return false',function(){
-        assert.equal(false,userReg.firstNameReg(""));
+        assert.equal(false,userReg.userRegistration("","firstName"));
     }),
     it('given first name is undefined should return false',function(){
-        assert.equal(false,userReg.firstNameReg(undefined));
+        assert.equal(false,userReg.userRegistration(undefined,"firstName"));
     }),
     it('given first name is correct',function(){
-        assert.equal(true,userReg.firstNameReg("Pranali"));
+        assert.equal(true,userReg.userRegistration("Pranali","firstName"));
     }),
     it('given first name is in number should return false',function(){
-        assert.equal(false,userReg.firstNameReg("1234"));
+        assert.equal(false,userReg.userRegistration("1234","firstName"));
     }),
     it('given second letter of first name if capital should return false',function(){
-        assert.equal(false,userReg.firstNameReg("PLembhe"))
+        assert.equal(false,userReg.userRegistration("PLembhe","firstName"))
     }),
     it('given first letter of capital is small of first name should return false',function(){
-        assert.equal(false,userReg.firstNameReg("pranali"))
+        assert.equal(false,userReg.userRegistration("pranali","firstName"))
     }),
     it('given first name have less than three letters should return false',function(){
-        assert.equal(false,userReg.firstNameReg("Pr"))
+        assert.equal(false,userReg.userRegistration("Pr","firstName"))
     }),
     it('given first name contains number should return false',function(){
-        assert.equal(false,userReg.firstNameReg("Pra23"))
+        assert.equal(false,userReg.userRegistration("Pra23","firstName"))
     }),
     it('given first name should not contains special symbols',function(){
-        assert.equal(false,userReg.firstNameReg("Name@er"))
+        assert.equal(false,userReg.userRegistration("Name@er","firstName"))
     }),
     it('given first name should not contain spaces',function(){
-        assert.equal(false,userReg.firstNameReg("Pranali lembhe"))
+        assert.equal(false,userReg.userRegistration("Pranali lembhe","firstName"))
     })
 })
 
 
 describe('Last Name Validation',function(){
     it('given parameter of function is empty should return false',function(){
-        assert.equal(false,userReg.lastNameReg());
+        assert.equal(false,userReg.userRegistration());
     }),
     it('given last name is null should return false',function(){
-        assert.equal(false,userReg.lastNameReg(null));
+        assert.equal(false,userReg.userRegistration(null,"lastName"));
     }),
     it('given last name is empty should return false',function(){
-        assert.equal(false,userReg.lastNameReg(""));
+        assert.equal(false,userReg.userRegistration("","lastName"));
     }),
     it('given last name is undefined should return false',function(){
-        assert.equal(false,userReg.lastNameReg(undefined));
+        assert.equal(false,userReg.userRegistration(undefined,"lastName"));
     }),
     it('given last name is correct',function(){
-        assert.equal(true,userReg.lastNameReg("Pranali"));
+        assert.equal(true,userReg.userRegistration("Pranali","lastName"));
     }),
     it('given last name is in number should return false',function(){
-        assert.equal(false,userReg.lastNameReg("1234"));
+        assert.equal(false,userReg.userRegistration("1234","lastName"));
     }),
     it('given second letter of last name if capital should return false',function(){
-        assert.equal(false,userReg.lastNameReg("PLembhe"))
+        assert.equal(false,userReg.userRegistration("PLembhe","lastName"))
     }),
     it('given first letter of capital is small of last name should return false',function(){
-        assert.equal(false,userReg.lastNameReg("pranali"))
+        assert.equal(false,userReg.userRegistration("pranali","lastName"))
     }),
     it('given last name have less than three letters should return false',function(){
-        assert.equal(false,userReg.lastNameReg("Pr"))
+        assert.equal(false,userReg.userRegistration("Pr","lastName"))
     }),
     it('given last name contains number should return false',function(){
-        assert.equal(false,userReg.lastNameReg("Pra23"))
+        assert.equal(false,userReg.userRegistration("Pra23","lastName"))
     }),
     it('given last name should not contains special symbols',function(){
-        assert.equal(false,userReg.lastNameReg("Name@er"))
+        assert.equal(false,userReg.userRegistration("Name@er","lastName"))
     }),
     it('given last name should not contain spaces',function(){
-        assert.equal(false,userReg.lastNameReg("Pranali lembhe"))
+        assert.equal(false,userReg.userRegistration("Pranali lembhe","lastName"))
     })
 })
 
 describe('Email Validation',function(){
     it('given email should not be undefined',function(){
-        assert.equal(false,userReg.emailRegistration())
+        assert.equal(false,userReg.userRegistration())
     }),
     it('given email is null should return false',function(){
-        assert.equal(false,userReg.emailRegistration(null));
+        assert.equal(false,userReg.userRegistration(null,"email"));
     }),
     it('given email is empty should return false',function(){
-        assert.equal(false,userReg.emailRegistration(""));
+        assert.equal(false,userReg.userRegistration("","email"));
     }),
     it('given email is undefined should return false',function(){
-        assert.equal(false,userReg.emailRegistration(undefined));
+        assert.equal(false,userReg.userRegistration(undefined,"email"));
     }),
     it('given email is correct',function(){
-        assert.equal(true,userReg.emailRegistration("pranali.lembhe77@gmail.com"));
+        assert.equal(true,userReg.userRegistration("pranali.lembhe77@gmail.com","email"));
     }),
     it('given email is in number should return false',function(){
-        assert.equal(false,userReg.emailRegistration("1234"));
+        assert.equal(false,userReg.userRegistration("1234","email"));
     }),
     it('given email address should not contains special letter at starting position',function(){
-        assert.equal(false,userReg.emailRegistration("@pranali.lembhe@gmail.com"))
+        assert.equal(false,userReg.userRegistration("@pranali.lembhe@gmail.com","email"))
     }),
     it('given email should have contain a . + - or _ between two words',function(){
-        assert.equal(true,userReg.emailRegistration("pranali_lembhe@gmail.com"))
+        assert.equal(true,userReg.userRegistration("pranali_lembhe@gmail.com","email"))
     })
 })
 
 describe('Password Validation',function(){
     it('given password should not be undefined',function(){
-        assert.equal(false,userReg.passWordReg())
+        assert.equal(false,userReg.userRegistration())
     }),
     it('given password name is null should return false',function(){
-        assert.equal(false,userReg.passWordReg(null));
+        assert.equal(false,userReg.userRegistration(null,"password"));
     }),
     it('given password is empty should return false',function(){
-        assert.equal(false,userReg.passWordReg(""));
+        assert.equal(false,userReg.userRegistration("","password"));
     }),
     it('given password is undefined should return false',function(){
-        assert.equal(false,userReg.passWordReg(undefined));
+        assert.equal(false,userReg.userRegistration(undefined,"password"));
     }),
     it('given password name is correct',function(){
-        assert.equal(true,userReg.passWordReg("Pranali#123"));
+        assert.equal(true,userReg.userRegistration("Pranali#123","password"));
     }),
     it('given password is in number should return false',function(){
-        assert.equal(false,userReg.passWordReg("1234"));
+        assert.equal(false,userReg.userRegistration("1234","password"));
     }),
     it('given password contains no special letter should return false',function(){
-        assert.equal(false,userReg.passWordReg("pranali122"))
+        assert.equal(false,userReg.userRegistration("pranali122","password"))
     })
 })
